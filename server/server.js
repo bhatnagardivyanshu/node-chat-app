c4f1150ab43a6d4ebc07bd5de9c6168d4ecce5aa
Divyanshu Bhatnagar <bhatnagardivyanshu@gmail.com>
2018-12-08 01:22:06 +0530

    ADD greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,14 +15,34 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New connection received');
 
+    // send only to the new user, a welcome message
     socket.emit('newMessage', {
-        from: 'Divyanshu',
-        text: 'Hey there!',
+        from: 'Application',
+        text: 'Welcome user!',
         createdAt: new Date().getTime()
     })
+    
+    socket.broadcast.emit('newMessage', {
+        from: 'Application',
+        text: 'New user joined!',
+        createdAt: new Date().getTime()
+    })
+    
+    // socket.emit('newMessage', {
+    //     from: 'Divyanshu',
+    //     text: 'Hey there!',
+    //     createdAt: new Date().getTime()
+    // })
 
-    socket.on('createMessage', (data) => {
-        console.log('New message created', data);
+    socket.on('createMessage', (message) => {
+        // console.log('New message created', message);
+
+        // send message to all the open connections
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     // socket.on('disconnect', () => {
