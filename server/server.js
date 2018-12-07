d98582cdf8b0dcad7fe4ec41c173f9c09a49b3eb
Divyanshu Bhatnagar <bhatnagardivyanshu@gmail.com>
2018-12-07 17:56:28 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express')
+const app = express();
+
+const publicPath = path.resolve(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
